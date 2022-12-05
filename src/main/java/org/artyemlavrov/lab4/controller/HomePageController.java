255857ad0bd38359cdddc6c8f5f2dc5ca98f5cae
Anton-Shabalov <anton.shabalov@icloud.com>
2022-12-05 20:43:16 +0300

    Комментирование упоминания не успользуемых классов (для дальнейшей корректировки в существующие entity)

--- src/main/java/org/artyemlavrov/lab4/controller/HomePageController.java
+++ src/main/java/org/artyemlavrov/lab4/controller/HomePageController.java
@@ -1,7 +1,6 @@
 package org.artyemlavrov.lab4.controller;
 
-import org.artyemlavrov.lab4.entity.HistoryItem;
-import org.artyemlavrov.lab4.entity.User;
+
 import org.artyemlavrov.lab4.repository.HistoryRepository;
 import org.artyemlavrov.lab4.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,24 +24,24 @@ public class HomePageController {
     @Autowired
     private HistoryRepository historyRepository;
 
-    @RequestMapping(value = "/get_history", method = RequestMethod.GET, headers = "Accept=application/json")
-    @ResponseBody
-    public List<HistoryItem> getHistory() {
-        final User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return user.getHistory();
-    }
+//    @RequestMapping(value = "/get_history", method = RequestMethod.GET, headers = "Accept=application/json")
+//    @ResponseBody
+//    public List<HistoryItem> getHistory() {
+//        final User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        return user.getHistory();
+//    }
 
-    @RequestMapping(value = "/add_history_item", method = RequestMethod.POST, headers = "Accept=application/json")
-    @ResponseBody
-    public void addHistoryItem(@Valid HistoryItem item) {
-        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-        Set<ConstraintViolation<HistoryItem>> violations = validator.validate(item);
-        if (violations.isEmpty()) {
-            historyRepository.save(item);
-            final String username = SecurityContextHolder.getContext().getAuthentication().getName();
-            final User user = userRepository.getByUsername(username);
-            user.getHistory().add(item);
-            userRepository.save(user);
-        }
-    }
+//    @RequestMapping(value = "/add_history_item", method = RequestMethod.POST, headers = "Accept=application/json")
+//    @ResponseBody
+//    public void addHistoryItem(@Valid HistoryItem item) {
+//        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+//        Set<ConstraintViolation<HistoryItem>> violations = validator.validate(item);
+//        if (violations.isEmpty()) {
+//            historyRepository.save(item);
+//            final String username = SecurityContextHolder.getContext().getAuthentication().getName();
+//            final User user = userRepository.getByUsername(username);
+//            user.getHistory().add(item);
+//            userRepository.save(user);
+//        }
+//    }
 }
